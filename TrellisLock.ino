7a563bcd838955a28ffb01985f93c58879ed2101
SmplMan <smp4488@aol.com>
2014-10-02 08:11:35 -0400

    Adding FPS enroll and pasword cleanup

--- TrellisLock.ino
+++ TrellisLock.ino
@@ -1,65 +1,40 @@
 //Trellis
 #include <Wire.h>
 #include <Adafruit_Trellis.h>
-Adafruit_Trellis trellis = Adafruit_Trellis();
-#define numKeys 16
 //Pass
 #include <avr/eeprom.h>
 #include <EEPROM.h>
+//FPS
+#include <FPS_GT511C3.h>
+#include <SoftwareSerial.h>
 
-int currentFlash = 0;
-int numTimes = 0;
-int currentInterval = 0;
-long previousMillis = 0;
+// Hardware setup - FPS connected to:
+//    digital pin 4(arduino rx, fps tx)
+//    digital pin 5(arduino tx - 1K resistor fps tx - 2K resistor - ground)
+//    this brings the 5v tx line down to about 3.2v so we dont fry our fps
 
-boolean flashDisplay = false;
-boolean flashOn = false;
-boolean acceptingCommands = false;
+FPS_GT511C3 fps(4, 5);
 
-void setupFlash(long interval, int times)
+//Trellis
+Adafruit_Trellis trellis = Adafruit_Trellis();
+#define numKeys 16
+
+void flash(long interval, int times)
 {
-  currentInterval = interval;
-  numTimes = times;
-  flashDisplay = true;
-}
-
-void flash()
-{
-  unsigned long currentMillis = millis();
-
-  if((currentMillis - previousMillis > currentInterval)) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    if (!flashOn) {
+  for(int i=0; i<times; i++){
       for (uint8_t i=0; i<numKeys; i++) {
         trellis.setLED(i);
       }
       trellis.writeDisplay();
-      flashOn = true;
-    } else {
+      delay(interval);
       trellis.clear();
       trellis.writeDisplay();
-      flashOn = false;
-      currentFlash += 1;
-    }
-  }
-
-  //Clear Vars and set controls
-  if(numTimes == currentFlash) {
-    flashDisplay = false;
-    currentFlash = 0;
-    numTimes = 0;
-    currentInterval = 0;
-    previousMillis = 0;
-    trellis.clear();
-    acceptingCommands = true;
+      delay(interval);
   }
 }
 
 int passCode = 15;
 int passTmp;
-boolean isPassSet;
 boolean isAuthenticated = false;
 
 void checkPasscode()
@@ -85,7 +60,7 @@ void checkPasscode()
         isAuthenticated = true;
 
         //Flash success
-        setupFlash(500, 2);
+        flash(500, 2);
       }
     }
   }
@@ -117,7 +92,7 @@ void setPassword()
   // EEPROM.write(0,1);
 }
 
-//boolean isPassSet = false;
+boolean isPassSet = false;
 boolean passSet = false;
 
 void getPassword()
@@ -136,6 +111,68 @@ void getPassword()
 void clearPassword()
 {
   //EEPROM.write(0,1);
+}
+
+boolean fpsEnrolling = false;
+
+void fpsEnroll()
+{
+  fps.SetLED(true);
+  delay(100);
+  // find open enroll id
+  int enrollid = 0;
+  bool usedid = true;
+  while (usedid == true)
+  {
+    usedid = fps.CheckEnrolled(enrollid);
+    if (usedid==true) enrollid++;
+  }
+  fps.EnrollStart(enrollid);
+
+  // enroll
+  Serial.print("Press finger to Enroll #");
+  Serial.println(enrollid);
+  while(fps.IsPressFinger() == false) delay(100);
+  bool bret = fps.CaptureFinger(true);
+  int iret = 0;
+  if (bret != false)
+  {
+    Serial.println("Remove finger");
+    fps.Enroll1();
+    while(fps.IsPressFinger() == true) delay(100);
+    Serial.println("Press same finger again");
+    while(fps.IsPressFinger() == false) delay(100);
+    bret = fps.CaptureFinger(true);
+    if (bret != false)
+    {
+      Serial.println("Remove finger");
+      fps.Enroll2();
+      while(fps.IsPressFinger() == true) delay(100);
+      Serial.println("Press same finger yet again");
+      while(fps.IsPressFinger() == false) delay(100);
+      bret = fps.CaptureFinger(true);
+      if (bret != false)
+      {
+        Serial.println("Remove finger");
+        iret = fps.Enroll3();
+        if (iret == 0)
+        {
+          Serial.println("Enrolling Successfull");
+        }
+        else
+        {
+          Serial.print("Enrolling Failed with error code:");
+          Serial.println(iret);
+        }
+      }
+      else Serial.println("Failed to capture third finger");
+    }
+    else Serial.println("Failed to capture second finger");
+  }
+  else Serial.println("Failed to capture first finger");
+
+  fpsEnrolling = false;
+  fps.SetLED(false);
 }
 
 long btnInterval = 3000;
@@ -167,7 +204,7 @@ void checkButtonHold()
       allow = false;
       // Serial.print(btnCurrent);
       // Serial.println(" Button held for 3 seconds");
-      setupFlash(500, 1);
+      flash(500, 1);
 
       //Bottom Left Button
       if(btnCurrent == 12) {
@@ -190,8 +227,13 @@ void checkButtonHold()
       if(btnCurrent == 0) {
         Serial.println("Locked");
         isAuthenticated = false;
-        acceptingCommands = false;
         passTmp = 0;
+      }
+
+      //Top Right Button
+      if(btnCurrent == 3){
+        Serial.println("Enrolling new finger");
+        fpsEnrolling = true;
       }
     }
   }
@@ -208,6 +250,13 @@ void setup()
   trellis.clear();
   trellis.writeDisplay();
 
+  flash(500, 2);
+
+  //FPS Setup
+  //fps.UseSerialDebug = true; // so you can see the messages in the serial debug screen
+  fps.Open();
+
+
   //Check if password is set
   //settingPass = true;
   //getPassword();
@@ -217,20 +266,20 @@ void loop()
 {
   delay(30);
 
-  if(!isAuthenticated || acceptingCommands) {
-    trellis.readSwitches();
-
-    if(settingPass) {
-      setPassword();
-      checkButtonHold();
-    }else if(!isAuthenticated) {
-      checkPasscode();
-    } else {
-      checkButtonHold();
-    }
+  trellis.readSwitches();
+  if(!isAuthenticated) {
+    checkPasscode();
+  }else if(settingPass) {
+    setPassword();
+    checkButtonHold();
+  } else {
+    checkButtonHold();
   }
 
-  if (flashDisplay) {
-    flash();
+  //Serial.print("fpsEnrolling= ");
+  //Serial.println(fpsEnrolling);
+  if(fpsEnrolling) {
+    fpsEnroll();
   }
+
 }
